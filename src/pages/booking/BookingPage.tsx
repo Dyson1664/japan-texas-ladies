571e15214dda5204ab3a35d0abecc2b8cc6ead11
Dyson1664 <127164393+Dyson1664@users.noreply.github.com>
2026-03-03 01:30:07 +0800

    updated to nrmal price

--- src/pages/booking/BookingPage.tsx
+++ src/pages/booking/BookingPage.tsx
@@ -56,7 +56,7 @@ const BOOKING_CONFIG: Record<
   japan: {
     countryName: "Japan",
     productHandle: "japan-2027-deposit", // <-- CHANGE if your handle is different eb:45272110235827 Continue save $100
-    variantId: "45272110235827", // <-- REQUIRED to skip product page and go straight to checkout st:45287018856627 Continue — Standard Price
+    variantId: "45287018856627", // <-- REQUIRED to skip product page and go straight to checkout
     requiresPassport: false,
     shopifyDomain: "tbff.imaginebeyondtravel.com",
   },
@@ -611,7 +611,7 @@ export default function BookingPage2() {
                     ? "Payments Opening Soon"
                     : isSubmitting
                     ? "Redirecting..."
-                    : "Continue save $100"}
+                    : "Continue Standard Price"}
                 </Button>
 
                 {submitError ? (
@@ -641,6 +641,8 @@ export default function BookingPage2() {
     </div>
   );
 }
+
+
 
 
 
